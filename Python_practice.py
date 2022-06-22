89a144d98328442db774c683869861eeedc35106
Yuyuhita Guzman <yuyuhita.guzman@gmail.com>
2022-06-21 22:10:23 -0400

    Adding the election audit code

---
+++ Python_practice.py
@@ -0,0 +1,29 @@
+
+# counties = ["Arapahoe","Denver","Jefferson"]
+# if "Arapahoe" in counties and "El Paso" not in counties:
+#    print("Only Arapahoe is in the list of counties.")
+# else:
+#     print("Arapahoe is in the list of counties and El Paso is not in the list of counties.")
+
+# voting_data = []
+# voting_data.append({"county":"Arapahoe", "registered_voters": 422829})
+# voting_data.append({"county":"Denver", "registered_voters": 463353})
+# voting_data.append({"county":"Jefferson", "registered_voters": 432438})
+
+# voting_data
+
+# counties = ["Arapahoe","Denver","Jefferson"]
+# if counties[1] == 'Denver':
+#     print(counties[1])
+
+# temperature = int(input("What is the temperature outside? "))
+# if temperature > 80:
+#    print("Turn on the AC.")
+# else:
+#    print("Open the windows.")
+
+counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties.")
